0b5ac404d5133cd8b15f3eab586d5ede72cb12fb
maxwatsonthecoder <140012390+maxwatsonthecoder@users.noreply.github.com>
2024-10-25 01:20:19 -0500

    Make dynamic grid size button functions. Need color randomizer and other choices. Make size radio selections.

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 const containerDiv = document.getElementById("container");
 const containerWidth = 600;
-let columns = 10;
-let squareSize = containerWidth / columns;
+const btn16 = document.getElementById("btn16");
+const btn32 = document.getElementById("btn32");
+const btn64 = document.getElementById("btn64");
 
+let columns;
+let squareSize;
+let countOfSquares;
+
+document.addEventListener('DOMContentLoaded', btn16Click);
+//loads 16x16 grid at startup
+
+function btn16Click() {
+    columns = 16;
+    countOfSquares = columns * columns;
+    squareSize = containerWidth / columns;
+    createGrid();
+}
+
+function btn32Click() {
+    columns = 32;
+    countOfSquares = columns * columns;
+    squareSize = containerWidth / columns;
+    createGrid();
+}
+
+function btn64Click() {
+    columns = 64;
+    countOfSquares = columns * columns;
+    squareSize = containerWidth / columns;
+    createGrid();
+}
 
 function createGrid() {
-    //need to create a for loop that creates 16(256), 32(1024), 64x(4096) divs
-    for (let i = 0; i < 100; i++) {
+    containerDiv.innerHTML = "";
+
+    for (let i = 0; i < countOfSquares; i++) {
         let square = document.createElement("div");
         square.classList.add("gridSquare");
         containerDiv.appendChild(square);
         square.style.flexBasis = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
 
         square.addEventListener("mouseover", function() {
             square.style.backgroundColor = "red";
 
-        })
+        });
 
 
        
@@ -25,4 +55,9 @@ function createGrid() {
 
 createGrid();
 
+document.getElementById("btn16").addEventListener("click", btn16Click);
+document.getElementById("btn32").addEventListener("click", btn32Click);
+document.getElementById("btn64").addEventListener("click", btn64Click);
 
+// Need to add dynamic code for bg color options, 
+//and make random color fn
